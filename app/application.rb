00ec46a6de7d51146c3f87abb0e54ecb0ef8ae8a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-17 08:30:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    req = Rack::Response.new(env)
+    req = Rack::Request.new(env)
 
     if req.path=="/<ITEM NAME>"
       resp.write "You requested this item"
