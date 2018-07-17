6de22836d0b0c61ad2cd04a5b41b2ac82b541a70
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-17 08:32:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,7 @@ class Application
 
     if req.path=="/ITEM NAME"
       resp.write "You requested this item"
+      resp.write "The price is £3.42"
     else
       resp.write "Route not found"
       resp.status = 400
