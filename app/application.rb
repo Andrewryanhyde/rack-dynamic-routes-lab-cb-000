521b8919f9f848fe69dcd98292efc80d7570ee41
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-17 08:29:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,12 @@
-class Application 
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Response.new(env)
 
-
+    if req.path=='/item/<ITEM NAME>'
+      resp.write "You requested this item"
+    else 
+      resp.write "Page not found"
+      resp.status = 404
+    end
 end
